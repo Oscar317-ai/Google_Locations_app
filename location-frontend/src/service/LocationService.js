09e9eece8b398f5f20c8ef9eaa18d32e090f2f22
oscar <oscar@kali.oscar.com>
2024-08-05 14:08:52 +0300

    Initial commit

--- location-frontend/src/service/LocationService.js
+++ location-frontend/src/service/LocationService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 class LocationService {
 
     static BASE_URL = "http://localhost:8080";
-    static API_KEY = "AIzaSyA4piTdQgvmV0PTkKec9NnEK1lMNvW9aaM";
+    static API_KEY = "the key";
 
     static async getLocations() {
         const response = await axios.get(`${LocationService.BASE_URL}/location/get-all-locations`);
